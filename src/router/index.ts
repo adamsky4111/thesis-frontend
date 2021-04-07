15d12e4c98fa7d484518894bd4a49fedc1aea5d8
adamsky4111 <adamdz411@gmail.com>
2021-04-07 22:17:18 +0200

    modify app component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,46 @@
 import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import VueRouter from "vue-router";
+import routes from "@/router/routes/index";
+import store from "@/store";
 
 Vue.use(VueRouter);
-
-const routes: Array<RouteConfig> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  console.log();
+  console.log(to.matched.some((record) => console.log(record)));
+  if (to.matched.some((record) => record.meta.auth)) {
+    if (to.matched.some((record) => record.meta.role)) {
+      // if required role is specified
+      const userRoles = store.getters["auth/roles"];
+
+      const userHasRole = to.matched.some((record) => {
+        let access = false;
+        record.meta.role.forEach((role) => {
+          if (role && userRoles.includes(role)) {
+            access = true;
+          }
+        });
+        return access;
+      });
+
+      if (userHasRole) {
+        // if user has required role;
+        next();
+      } else {
+        next({ path: "/login" });
+      }
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;

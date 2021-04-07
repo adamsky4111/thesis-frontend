a826dc718b3ec146b117b6e5bc506999eee437fb
adamsky4111 <adamdz411@gmail.com>
2021-04-07 22:15:10 +0200

    add models

---
+++ src/model/UserRegisterModel.ts
@@ -0,0 +1,26 @@
+import { UserModel } from "@/model/UserModel";
+import { IValidator } from "@/model/IValidator";
+import validators from "@/validators/common";
+
+export class UserRegisterModel extends UserModel implements IValidator {
+  public repeatPassword?: string;
+  public validator: Record<never, Array<never>>;
+
+  constructor() {
+    super();
+    this.validator = {
+      email: validators.email,
+      username: validators.empty.concat(validators.noSpace),
+      password: validators.empty,
+      repeatPassword: validators.empty.concat([
+        () =>
+          this.repeatPassword === this.password || "Hasła muszą być identyczne",
+      ]),
+      about: [],
+      country: [],
+      nick: validators.noSpace,
+      firstName: validators.firstUpper,
+      lastName: validators.firstUpper,
+    };
+  }
+}

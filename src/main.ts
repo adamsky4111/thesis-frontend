3ee9bc4fc30f2d2d12bb129afcb0671f564d2d4f
adamsky4111 <adamdz411@gmail.com>
2021-04-11 19:49:53 +0200

    add scss

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import translations from "./plugins/translations";
+import "./style/main.scss";
 
 Vue.config.productionTip = false;
 

ffcdee974ec164b8a8b0fae36c2f87047364fe4a
adamsky4111 <adamdz411@gmail.com>
2021-04-12 20:06:15 +0200

    add store refresh token, avatar change

--- src/store/modules/auth/mutations.ts
+++ src/store/modules/auth/mutations.ts
@@ -7,6 +7,8 @@ export type Mutations<S = State> = {
   [MutationTypes.SET_ROLES](state: S, payload: Array<string>): void;
   [MutationTypes.SET_TOKEN](state: S, payload: string): void;
   [MutationTypes.SET_USER](state: S, payload: UserModel): void;
+  [MutationTypes.SET_AVATAR](state: S, payload: string): void;
+  [MutationTypes.SET_REFRESH_TOKEN](state: S, payload: string): void;
 };
 
 export const mutations: MutationTree<State> & Mutations = {
@@ -18,5 +20,11 @@ export const mutations: MutationTree<State> & Mutations = {
   },
   [MutationTypes.SET_USER](state, payload: UserModel) {
     state.user = payload;
+  },
+  [MutationTypes.SET_AVATAR](state, payload: string) {
+    state.user.avatar = payload;
+  },
+  [MutationTypes.SET_REFRESH_TOKEN](state, payload: string) {
+    state.refreshToken = payload;
   },
 };

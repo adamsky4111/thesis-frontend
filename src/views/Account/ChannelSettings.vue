5ba4eb81d4fc494226401c1954709361f876e4a5
adamsky4111 <adamdz411@gmail.com>
2021-04-25 11:46:24 +0200

    add settings channel page

---
+++ src/views/Account/ChannelSettings.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="account-settings">
+    <v-row>
+      <v-col align="center">
+        <h1>{{ $translation("account.settings.header") }}</h1>
+        <settings-table />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import AccountMenu from "@/components/user/AccountMenu.vue";
+import SettingsTable from "@/components/user/SettingsTable.vue";
+
+@Component({
+  components: { AccountMenu, SettingsTable },
+})
+export default class ChannelSettings extends Vue {}
+</script>
